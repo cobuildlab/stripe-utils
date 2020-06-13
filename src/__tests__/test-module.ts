6a79399d599bf0f2b747ed5e0a8d8e231197f2a6
carlos <kikeztw@gmail.com>
2020-06-13 19:15:15 -0400

    add init and withStripe

--- src/__tests__/test-module.ts
+++ src/__tests__/test-module.ts
@@ -1,5 +1,13 @@
-import { test, expect } from '@jest/globals';
+import { test, expect, describe } from '@jest/globals';
+import * as stripe from '../init';
 
-test('Test', () => {
-  expect(1).toBe(1);
+describe('Stripe test', () => {
+  test('Create customer', () => {
+    const testKey = 'sk_test_s982HH7vmGNQ5i4Fbtx5kOom00LBGWlBs0';
+    stripe.init(testKey);
+    expect(stripe.withStripe((e: any) => e)).toHaveProperty(
+      '_api.auth',
+      `Bearer ${testKey}`,
+    );
+  });
 });
--- src/init.ts
+++ src/init.ts
@@ -2,15 +2,18 @@ import Stripe from 'stripe';
 
 let _client: Stripe | null = null;
 
-
-export const init = (apiToken: string, config: Stripe.StripeConfig) => {
-  _client = new Stripe(apiToken, config);
+export const init = (apiToken: string, config?: Stripe.StripeConfig): void => {
+  const settings: Stripe.StripeConfig = config || {
+    apiVersion: '2020-03-02',
+  };
+  _client = new Stripe(apiToken, settings);
 };
-
 
 export const withStripe = (fn: Function) => {
   if (!_client) {
-    throw Error('Stripe instance not initialized, please execute "init" function first');
+    throw Error(
+      'Stripe instance not initialized, please execute "init" function first',
+    );
   }
-  return fn(_client); 
+  return fn(_client);
 };
