2df96db5bb97de47aa20972375defffe093529bb
carlos <kikeztw@gmail.com>
2020-06-13 22:58:02 -0400

    minor changes

--- src/__tests__/test-module.ts
+++ src/__tests__/test-module.ts
@@ -2,9 +2,12 @@ import { test, expect, describe } from '@jest/globals';
 import * as stripe from '../init';
 
 describe('Stripe test', () => {
-  test('Create customer', () => {
+  test('init test', () => {
+    // this api key is not real
     const testKey = 'sk_test_s982HH7vmGNQ5i4Fbtx5kOom00LBGWlBs0';
+
     stripe.init(testKey);
+
     expect(stripe.withStripe((e: any) => e)).toHaveProperty(
       '_api.auth',
       `Bearer ${testKey}`,
